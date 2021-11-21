38ab994a61d16c3ebd8ca3080b52835612fb94a8
usrbinkat <usrbinkat@braincraft.io>
2021-11-20 21:26:50 -0800

    refactor fedora
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

---
+++ preview/fedora/x86/34/customize.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -x
+#mkdir /etc/skel/.ssh
+#dnf update -y
+#dnf install -y git-core vim tmux net-tools screenfetch qemu-guest-agent
+#systemctl enable qemu-guest-agent
+#dnf clean all
+#rm -rf /var/cache/dnf*
