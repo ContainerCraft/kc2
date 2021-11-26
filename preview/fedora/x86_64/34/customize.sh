163023e7fa1e0b34b857ce405d1c294c3bcb9120
jbpratt <jbpratt78@gmail.com>
2021-11-26 05:28:21 -0600

    remove all image building
    image building is being lifted into the ccio/kmi repository
    
    updates #24

--- preview/fedora/x86_64/34/customize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-#mkdir /etc/skel/.ssh
-#dnf update -y
-#dnf install -y git-core vim tmux net-tools screenfetch qemu-guest-agent
-#systemctl enable qemu-guest-agent
-#dnf clean all
-#rm -rf /var/cache/dnf*
