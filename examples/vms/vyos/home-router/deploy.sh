fec7226d6fb44714ef8df17f142e7552cf2b84dc
usrbinkat <usrbinkat@braincraft.io>
2022-05-15 00:10:10 -0700

    enhance vyos home-router

--- examples/vms/vyos/home-router/deploy.sh
+++ examples/vms/vyos/home-router/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 
-kubectl create secret generic wan0vyos-cloudconfig --dry-run=client -n kargo --from-file=userdata=cloud-config.userdata -oyaml | kubectl apply -f -
+kubectl create secret generic wan0vyos-cloudconfig --dry-run=client -n default --from-file=userdata=cloud-config.userdata -oyaml | kubectl apply -f -
 
-kubectl apply -n kargo -f $1
+kubectl apply -n default -f $1
